b0b36416cc68ea4739b9ffe7fc5117716a4dcc82
JajaDiMarunong <jdconstantino5281@gmail.com>
2024-08-25 00:02:50 +0800

    Add files via upload

---
+++ MATHMATH.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class LabMath {
+    static void Main() {
+        Console.WriteLine("Enter your grades: ");
+        double[] grades = new double[5];
+
+        for (int i = 0; i < grades.Length; i++) {
+            grades[i] = double.Parse(Console.ReadLine());
+        }
+
+        double average = grades.Average();
+        int roundedAverage = (int)Math.Round(average);
+
+        Console.WriteLine("The average is " + average + " and rounded off to " + roundedAverage);
+    }
+}
